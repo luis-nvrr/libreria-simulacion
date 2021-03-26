d7916094c2d4ca20434e8820e206d9f867fbe9db
luis navarro <luisfnvrr@gmail.com>
2021-03-25 22:13:07 -0300

    arregla sobrecarga de metodo generarSerie en pantallaPrueba

--- Graficador/Prueba-Grafico-Generador.cs
+++ Graficador/Prueba-Grafico-Generador.cs
@@ -28,25 +28,25 @@ namespace Numeros_aleatorios.ejemplo_grafico
 
         private void Prueba_Grafico_Generador_Load(object sender, EventArgs e)
         {
-            excel = new GraficadorExcelObservado();
-            truncador = new Truncador(4);
+            //excel = new GraficadorExcelObservado();
+            //truncador = new Truncador(4);
 
-            intervalos = new GeneradorIntervalosUniforme(truncador);
-            intervalos.generarIntervalos(20);
-            float[] inicio = intervalos.obtenerInicioIntervalos();
-            float[] fin = intervalos.obtenerFinIntervalos();
+            //intervalos = new GeneradorIntervalosUniforme(truncador);
+            //intervalos.generarIntervalos(20);
+            //float[] inicio = intervalos.obtenerInicioIntervalos();
+            //float[] fin = intervalos.obtenerFinIntervalos();
 
-            frecuenciaObservada = new FrecuenciaObservada(inicio, fin);
-            frecuenciaEsperada = new FrecuenciaEsperadaUniforme(100, inicio, fin);
+            //frecuenciaObservada = new FrecuenciaObservada(inicio, fin);
+            //frecuenciaEsperada = new FrecuenciaEsperadaUniforme(100, inicio, fin);
            
-            lineal = new GeneradorCongruencialLinealMixto(truncador, 17,21,13,32); // parametros de la congruencial
-            float[] aleatorios = lineal.generarSerie(100, frecuenciaObservada);
-            int[] frecuenciasObservadas = frecuenciaObservada.obtenerFrecuencias();
-            int[] frecuenciasEsperadas = frecuenciaEsperada.obtenerFrecuencias();
+            //lineal = new GeneradorCongruencialLinealMixto(truncador, 17,21,13,32); // parametros de la congruencial
+            //float[] aleatorios = lineal.generarSerie(100, frecuenciaObservada);
+            //int[] frecuenciasObservadas = frecuenciaObservada.obtenerFrecuencias();
+            //int[] frecuenciasEsperadas = frecuenciaEsperada.obtenerFrecuencias();
 
-            //excel.frecuenciaEsperada = frecuenciasEsperadas;
-            excel.frecuenciaObservada = frecuenciasObservadas;
-            excel.ShowDialog();
+            ////excel.frecuenciaEsperada = frecuenciasEsperadas;
+            //excel.frecuenciaObservada = frecuenciasObservadas;
+            //excel.ShowDialog();
         }
     }
 }
